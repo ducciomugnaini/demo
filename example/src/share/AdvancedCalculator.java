d035b6fb86b8923f8de413da775415d85ec00471
Duccio Mugnaini <duccio.mugnaini@gmail.com>
2017-11-13 23:20:26 +0100

    canlc

---
+++ example/src/share/AdvancedCalculator.java
@@ -0,0 +1,22 @@
+package share;
+
+public class AdvancedCalculator {
+
+	double a;
+	double b;
+	
+	public AdvancedCalculator(double a, double b)
+	{
+		this.a = a;
+		this.b = b;
+	}
+	
+	public double Pow() {
+		return Math.pow(a, b);
+	}
+	
+	public double Mul()
+	{
+		return a*b;
+	}
+}
